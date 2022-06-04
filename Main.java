bbb889b1ad340ef3e7be7173700060a3e37dac12
kvitta <98219967+kvitta@users.noreply.github.com>
2022-06-04 21:51:17 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Lesson_7.Project;
+
+public class Main {
+
+        public static void main(String[] args) {
+            UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+            userInterfaceView.runInterface();
+        }
+}
